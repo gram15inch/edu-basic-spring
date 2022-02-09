09362084ee04ee8981dfff4aa9dcf962d7a25099
gram15inch <leapthestairs@gmail.com>
2022-02-09 20:39:41 +0900

    Delete DBinfo.java

--- test2/src/main/java/com/example/test2/DBinfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.test2;
-
-public class DBinfo {
-    public final static String DRIVER ="com.mysql.jdbc.Driver";
-    public final static String URL = "jdbc:mysql://localhost/myspring?characterEncoding=UTF-8";
-    public final static String USERNAME = "root";
-    public final static String PASSWORD = "1248";
-}
