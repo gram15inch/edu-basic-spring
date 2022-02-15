400709966111d18b39966da9644cb8adc7159178
gram15inch <leapthestairs@gmail.com>
2022-02-16 03:52:36 +0900

    2/15

--- test2/src/main/java/com/example/test2/dao/UserDaoTest.java
+++ test2/src/main/java/com/example/test2/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package com.example.test2.dao;
 
 import com.example.test2.domain.User;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,27 +11,38 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-       // ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+    @Test
+    public void addAndGet() throws SQLException{
+        //ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         //ApplicationContext context = new ClassPathXmlApplicationContext("daoContext.xml",UserDao.class);
         //ApplicationContext context = new ClassPathXmlApplicationContext();
         UserDao dao = context.getBean("userDao",UserDao.class);
 
         // user 생성
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("워익워익");
+        user.setName("김띠용");
+        user.setPassword("tytywiwi");
 
         // user 추가
-        dao.add(user);
+        //dao.add(user);
 
         // dao 실행
-        System.out.println(user.getId());
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
+        //User user2 = dao.get(user.getId());
+        User user2 = new User();
+        user2.setId("워익워익");
+        user2.setName("김띠용");
+        user2.setPassword("tytywiwi");
+
+        // test
+        assertThat(user2.getName(),is(user.getName()));
+        assertThat(user2.getPassword(),is(user.getPassword()));
+
     }
 }
