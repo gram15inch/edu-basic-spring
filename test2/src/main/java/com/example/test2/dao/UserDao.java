9f98650ed8b9acb8122453f4db57fd701f8a0357
gram15inch <leapthestairs@gmail.com>
2022-02-25 17:42:46 +0900

    2/25

--- test2/src/main/java/com/example/test2/dao/UserDao.java
+++ test2/src/main/java/com/example/test2/dao/UserDao.java
@@ -5,7 +5,9 @@ package com.example.test2.dao;
 
 import com.example.test2.DBinfo;
 import com.example.test2.domain.User;
+import com.mysql.cj.exceptions.MysqlErrorNumbers;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
@@ -23,40 +25,64 @@ import java.util.List;
 
 
 public class UserDao {
+    private DataSource dataSource; // 필요없음
 
     private JdbcTemplate jdbcTemplate;
-    private DataSource dataSource;
+    private RowMapper<User> userMapper =  new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
 
     public void setDataSource(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
         this.dataSource = dataSource;
     }
 
-    
-    public void add(final User user) throws SQLException {
-        jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)"
-                ,user.getId()
-                ,user.getName()
-                ,user.getPassword());
+
+    public void add(final User user) throws DuplicateKeyException, SQLException {
+            jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)"
+                    , user.getId()
+                    , user.getName()
+                    , user.getPassword());
+
     }
 
     public User get(String id) throws  SQLException {
-
         return this.jdbcTemplate.queryForObject("select * from users where id = ?",
                 new Object[]{id},
-                new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-                });
+                this.userMapper);
+    }
 
+    public void deleteAll() throws SQLException {
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() throws SQLException {
+        return this.jdbcTemplate.queryForObject("select count(*) from users",Integer.class);
 
     }
+
+    public List<User> getAll(){
+        return jdbcTemplate.query("select * from users order by id",
+                this.userMapper);
+
+    }
+
+
+
+
+
+
+
+
+
+
     public User getBefore(String id) throws  SQLException {
 
 
@@ -102,16 +128,6 @@ public class UserDao {
 
 
     }
-
-    public void deleteAll()throws SQLException{
-        this.jdbcTemplate.update("delete from users");
-    }
-
-    public int getCount() throws SQLException {
-        return this.jdbcTemplate.queryForObject("select count(*) from users",Integer.class);
-
-    }
-
     public int getCountBefore() throws SQLException{
 
         Connection c = null;
@@ -141,21 +157,6 @@ public class UserDao {
         }
 
 
-    }
-
-    public List<User> getAll(){
-        return jdbcTemplate.query("select * from users order by id",
-                new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-                });
-        //263p 까지 복습
     }
 }
 
