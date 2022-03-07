f1a5ee55da389e335779ae071f20d37f9a5271f4
gram15inch <leapthestairs@gmail.com>
2022-03-07 21:18:33 +0900

    3/7

--- test2/src/main/java/com/example/test2/service/UserServiceTest.java
+++ test2/src/main/java/com/example/test2/service/UserServiceTest.java
@@ -38,7 +38,7 @@ public class UserServiceTest {
         users = Arrays.asList(
                 new User("bumjin", "박범진", "p1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER-1,0),
                 new User("joytouch", "강명성", "p2", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
-                new User("erwins", "신승한", "P3", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD-1),
+                new User("erwins", "신승한", "P3  ", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD-1),
                 new User("madnite1", "이상호", "P4", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD),
                 new User("green", "오민규", "P5", Level.GOLD, 100, 100)
         );
@@ -98,15 +98,16 @@ public class UserServiceTest {
 
     @Test
     public void upgradeAllOrNothing() throws Exception{
-        UserService testUserService = new TestUserService(users.get(3).getId());
-        testUserService.setUserDao(this.userDao);
         UserLevelUpgradePolicyDefault p = new UserLevelUpgradePolicyDefault();
         p.setUserDao(this.userDao);
+
+        UserService testUserService = new TestUserService(users.get(3).getId());
         testUserService.setUserLevelUpgradePolicy(p);
+        testUserService.setUserDao(this.userDao);
         testUserService.setDataSource(this.dataSource);
 
-        userDao.deleteAll();
-        for(User user : users) userDao.add(user);
+
+        userDao.deleteAll();       for(User user : users) userDao.add(user);
 
         try{
             testUserService.upgradeLevels();
